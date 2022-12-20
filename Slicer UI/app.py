666783804328a1519970f5bd9049a1ca0286a541
Sevi <sevastiyan.tsv@gmail.com>
2022-12-20 16:46:09 +0900

    Slicer UI

--- Slicer UI/app.py
+++ Slicer UI/app.py
@@ -22,7 +22,7 @@ class App(customtkinter.CTk):
         self.title("CustomTkinter")
         self.index = 0
         self.data = data
-        self.results = []
+        self.results = {'acc': [], 'gyro': []}
         self.create_widgets()
 
     def create_widgets(self):
@@ -34,10 +34,14 @@ class App(customtkinter.CTk):
         self.frame_1 = customtkinter.CTkFrame(master=self)
         self.frame_1.grid(row=1, column=0, rowspan=3, padx=20, pady=20, sticky="ew")
 
-        self.button_slice = customtkinter.CTkButton(master=self, text="Slice", fg_color="green", command=self.save_slice)
+        self.button_slice = customtkinter.CTkButton(
+            master=self, text="Slice", fg_color="green", command=self.save_slice
+        )
         self.button_slice.grid(row=1, column=1, padx=20, pady=5, sticky="ew")
 
-        self.button_skip = customtkinter.CTkButton(master=self, text="Skip", fg_color="orange", command=self.update_figure)
+        self.button_skip = customtkinter.CTkButton(
+            master=self, text="Skip", fg_color="orange", command=self.update_figure
+        )
         self.button_skip.grid(row=2, column=1, padx=20, pady=5, sticky="ew")
 
         self.button_save = customtkinter.CTkButton(master=self, text="Save", command=self.save_file)
@@ -55,65 +59,82 @@ class App(customtkinter.CTk):
         self.update_figure()  # has progress dependency.
 
         self.slider_1 = customtkinter.CTkSlider(
-            master=self.frame_1, command=self.move_slice, from_=self.t[0], to=self.t[-1]  # has t dependency from update_figure
+            master=self.frame_1,
+            command=self.move_slice,
+            from_=0,
+            to=1,  # has t dependency from update_figure
         )
         self.slider_1.pack(pady=12, padx=10)
-        self.slider_1.set(len(self.t) / 2)
+        self.slider_1.set(0.5)#len(self.t) / 2)
 
     def update_figure(self):
+        print('------------------')
         self.ax1.clear()
         self.ax2.clear()
-        self.signal = self.get_signal()
-        self.t = [x for x in range(self.signal.shape[0])]
-        (self.line,) = self.ax1.plot(self.t, self.signal)
+        self.acc_signal, self.gyro_signal = self.get_signal()
+        self.t = range(self.acc_signal.shape[0])
+        (self.line,) = self.ax1.plot(self.t, self.acc_signal)
         borders = (len(self.t) / 2, len(self.t) / 2 + 200)
         self.left_border = self.ax1.axvline(borders[0], color='r')
         self.right_border = self.ax1.axvline(borders[1], color='r')
-        (self.line2,) = self.ax2.plot(self.t, self.signal)
+        (self.line2,) = self.ax2.plot(self.t, self.acc_signal)
         self.ax2.set_xlabel("time [s]")
         self.ax1.set_ylabel("f(t)")
-        print(max(self.line.get_data()[1]))
+        print('Max value: ', max(self.line.get_data()[1]))
         self.update_progress()
         self.canvas.draw()
 
     def get_signal(self):
         sample = self.data[self.index]
-        signal = convert_signal(sample, type='acc_total')
-        signal = signal.to_numpy()
-        print(f'index {self.index}, shape {signal.shape}')
-        self.index += 1
+        acc = convert_signal(sample, type='acc_total')
+        gyro = convert_signal(sample, type='gyro_total')
+        acc = acc.to_numpy()
+        gyro = gyro.to_numpy()
+        print(f'index: {self.index}')
+        print(f'Acc shape {acc.shape}', f' Gyro shape {gyro.shape}')
 
-        return signal
+        return acc, gyro
 
     def move_slice(self, new_val):
-        self.left_border.set_xdata(new_val)
-        self.right_border.set_xdata(new_val + 200)
-        self.ax2.set_xlim(new_val - 20, new_val + 220)
+        x = int(len(self.t) * new_val) # convert from fraction to data where t is x axis and new_val is 
+        self.left_border.set_xdata(x)
+        self.right_border.set_xdata(x + 200)
+        self.ax2.set_xlim(x - 20, x + 220)
         self.canvas.draw()
 
     def update_progress(self):
+        self.index += 1  # ! Important to keep track of the current index of the file list
         length = len(self.data)
         progress = self.index / length
-        print(progress)
         self.progressbar_1.set(progress)
+        print('Progress: ', self.index, '/', length)
 
     def save_slice(self):
-        x = int(self.slider_1.get())
-        data_slice = self.signal[x:x+200]
-        if len(data_slice) < 200: # Padding
-            data_slice = np.resize(data_slice, (200))
+        x = int(self.slider_1.get() * len(self.t)) # convert from fraction to data where
+        acc_slice = self.acc_signal[x : x + 200]
+        if len(acc_slice) < 200:  # Padding
+            acc_slice = np.resize(acc_slice, (200))
+        self.results['acc'].append(acc_slice)
 
-        print(data_slice.shape) 
-        self.results.append(data_slice)
+        gyro_slice = self.gyro_signal[x : x + 200]
+        if len(gyro_slice) < 200:  # Padding
+            gyro_slice = np.resize(gyro_slice, (200))
+        self.results['gyro'].append(gyro_slice)
+
+        print('Slice Shape: ', acc_slice.shape)
         self.update_figure()
+        self.slider_1.set(0.5) # len(self.t) / 2)
 
     def save_file(self):
         path = 'data/processed/'
         if not os.path.isdir(path):
             os.makedirs(path)
 
-        with open(f"{path}/incidents.txt", "w") as f: #! Careful with Append
-            np.savetxt(f, self.results, fmt="%s", delimiter=" ")
+        with open(f"{path}/incidents_acc.txt", "w") as f:  #! Careful with Append
+            np.savetxt(f, self.results['acc'], fmt="%s", delimiter=" ")
+        with open(f"{path}/incidents_gyro.txt", "w") as f:  #! Careful with Append
+            np.savetxt(f, self.results['gyro'], fmt="%s", delimiter=" ")
+        print('Data Saved')
 
     def quit(self):
         self.save_file()
@@ -122,11 +143,12 @@ class App(customtkinter.CTk):
 
 def main():
     # ----- Data Load -----
-    root = 'data/incidents/'
+    root = 'data/incidents/' # /2022-12-06/Embeded Insole/Slip'
     file_list = []
     filenames = []
+
     for file in os.listdir(root):
-        if file in ['processed']:  # Skip folders
+        if file in ['processed', '2022-12-06']:  # Skip folders
             continue
         file_list.append(os.path.join(root, file))  # .replace("\\","/")
         filenames.append(file)
